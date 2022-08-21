80c56994be2f8f7004538ed6f67997d34de448fe
YasmeenG <yasmeen.grewal@gmail.com>
2022-08-21 15:20:38 -0400

    changed demo.py

--- alert.py
+++ alert.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-alertVariable = "Option3"
+alertVariable = "Option5"
 ser = Service("/usr/local/bin/chromedriver")
 
 driver = webdriver.Chrome(service=ser)
